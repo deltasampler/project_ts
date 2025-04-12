172b260c646bf7fa1f912f70d14a3b00dcfff226
deltasampler <deltasampler@gmail.com>
2025-04-12 19:05:56 +0300

    initial commit

---
+++ modules/clone.cjs
@@ -0,0 +1,23 @@
+const {execSync} = require("child_process");
+
+const commands = [
+    "git clone git@deltasampler:deltasampler/cl_ts.git cl",
+    "git clone git@deltasampler:deltasampler/driving.git",
+    "git clone git@deltasampler:deltasampler/engine_ts.git engine",
+    "git clone git@deltasampler:deltasampler/gui_ts.git gui",
+    "git clone git@deltasampler:deltasampler/panel_templater.git",
+    "git clone git@deltasampler:deltasampler/plant_gen.git",
+    "git clone git@deltasampler:deltasampler/pixel_sandbox.git",
+    "git clone git@deltasampler:deltasampler/ray_marcher.git",
+    "git clone git@deltasampler:deltasampler/simple_demos.git",
+    "git clone git@deltasampler:deltasampler/square.git",
+    "git clone git@deltasampler:deltasampler/turmite.git"
+];
+
+for (const command of commands) {
+    try {
+        execSync(command, {stdio: "inherit", shell: true});
+    } catch (error) {
+        console.error(error.message);
+    }
+}
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import {defineConfig} from "vite";
+import preact from "@preact/preset-vite";
+
+export default defineConfig({
+    plugins: [preact()],
+    resolve: {
+        alias: {
+            '@cl': path.resolve(__dirname, "./modules/cl/source"),
+            '@driving': path.resolve(__dirname, "./modules/driving/source"),
+            '@engine': path.resolve(__dirname, "./modules/engine/source"),
+            '@gui': path.resolve(__dirname, "./modules/gui/source"),
+            '@panel_templater': path.resolve(__dirname, "./modules/panel_templater/source"),
+            '@pixel_sandbox': path.resolve(__dirname, "./modules/pixel_sandbox/source"),
+            '@plant_gen': path.resolve(__dirname, "./modules/plant_gen/source"),
+            '@ray_marcher': path.resolve(__dirname, "./modules/ray_marcher/source"),
+            '@simple_demos': path.resolve(__dirname, "./modules/simple_demos/source"),
+            '@square': path.resolve(__dirname, "./modules/square/source"),
+            '@turmite': path.resolve(__dirname, "./modules/turmite/source")
+        }
+    }
+});
